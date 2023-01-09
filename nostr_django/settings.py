b4d7fadc8941e2494d392aa7577ed0dfe4eddc49
Rasmus Schlunsen <rasmus@schlunsen.com>
2023-01-09 20:56:26 +0100

    Use env vars

--- nostr_django/settings.py
+++ nostr_django/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = 'django-insecure-bhq09m=rarnt0qf#*)9t8)-1yi(*jtdkm48-o-nh--&5(oj2@p
 DEBUG = os.getenv('DEBUG', '1') == '1'
 
 DOMAIN = os.getenv('DOMAIN', 'http://localhost:8000')
-ALLOWED_HOSTS = [DOMAIN,]
+ALLOWED_HOSTS = ['https://{}'.format(DOMAIN),]
+CSRF_TRUSTED_ORIGINS = ['https://{}'.format(DOMAIN),]
 
 
 # Application definition
